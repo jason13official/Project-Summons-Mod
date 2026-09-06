234cf53412856101a773b11f0653bf44914c34cf
jason13official <jason13official@gmail.com>
2026-09-06 00:17:43 -0400

    traceable?

--- common/src/main/java/io/github/jason13official/summons/impl/common/entity/AbstractCompanion.java
+++ common/src/main/java/io/github/jason13official/summons/impl/common/entity/AbstractCompanion.java
@@ -2,12 +2,14 @@ package io.github.jason13official.summons.impl.common.entity;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.OwnableEntity;
 import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.TraceableEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
 
-public abstract class AbstractCompanion extends PathfinderMob {
+public abstract class AbstractCompanion extends PathfinderMob implements TraceableEntity {
 
   public AbstractCompanion(EntityType<? extends AbstractCompanion> entityType, Level level) {
     super(entityType, level);
