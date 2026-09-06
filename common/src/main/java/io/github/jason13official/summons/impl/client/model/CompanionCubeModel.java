b56e3507a7568593ceb2a0f8c9b59e79ef3d958e
jason13official <jason13official@gmail.com>
2026-09-06 17:18:46 -0400

    unused

--- common/src/main/java/io/github/jason13official/summons/impl/client/model/CompanionCubeModel.java
+++ common/src/main/java/io/github/jason13official/summons/impl/client/model/CompanionCubeModel.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import io.github.jason13official.summons.Summons;
 import io.github.jason13official.summons.impl.common.entity.AbstractCompanion;
-import io.github.jason13official.summons.impl.common.entity.ground.CompanionCube;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
