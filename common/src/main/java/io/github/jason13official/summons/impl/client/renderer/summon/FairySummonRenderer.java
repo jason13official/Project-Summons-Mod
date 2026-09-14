3be822da461ae249f0c43f9988de74f8e2e384fc
jason13official <jason13official@gmail.com>
2026-09-14 01:57:06 -0400

    textures fairy

--- common/src/main/java/io/github/jason13official/summons/impl/client/renderer/summon/FairySummonRenderer.java
+++ common/src/main/java/io/github/jason13official/summons/impl/client/renderer/summon/FairySummonRenderer.java
@@ -24,15 +24,17 @@ public class FairySummonRenderer extends EntityRenderer<AbstractCompanion> {
 
   static {
     TEXTURE_BY_FORM.put(Form.INFANT_FAIRY, DEFAULT_TEXTURE_LOCATION);
-    TEXTURE_BY_FORM.put(Form.LEAFFLE, Summons.identifier("textures/entity/summon/fairy_leaffle.png"));
-    TEXTURE_BY_FORM.put(Form.HERBEST, Summons.identifier("textures/entity/summon/fairy_herbest.png"));
-    TEXTURE_BY_FORM.put(Form.HONEY_BEE, Summons.identifier("textures/entity/summon/fairy_honey_bee.png"));
-    TEXTURE_BY_FORM.put(Form.KILLER_BEE, Summons.identifier("textures/entity/summon/fairy_killer_bee.png"));
-    TEXTURE_BY_FORM.put(Form.HORNET, Summons.identifier("textures/entity/summon/fairy_hornet.png"));
-    TEXTURE_BY_FORM.put(Form.PROBOSCIS_FAIRY, Summons.identifier("textures/entity/summon/fairy_proboscis_fairy.png"));
-    TEXTURE_BY_FORM.put(Form.TIRAMISU, Summons.identifier("textures/entity/summon/fairy_tiramisu.png"));
-    TEXTURE_BY_FORM.put(Form.TIARA, Summons.identifier("textures/entity/summon/fairy_tiara.png"));
-    TEXTURE_BY_FORM.put(Form.COMET_STAR, Summons.identifier("textures/entity/summon/fairy_comet_star.png"));
+    TEXTURE_BY_FORM.put(Form.LEAFFLE, Summons.identifier("textures/entity/summon/fairy/leaffle.png"));
+    TEXTURE_BY_FORM.put(Form.HERBEST, Summons.identifier("textures/entity/summon/fairy/herbest.png"));
+
+    // TODO below need actual textures
+    TEXTURE_BY_FORM.put(Form.HONEY_BEE, Summons.identifier("textures/entity/summon/fairy/honey_bee.png"));
+    TEXTURE_BY_FORM.put(Form.KILLER_BEE, Summons.identifier("textures/entity/summon/fairy/killer_bee.png"));
+    TEXTURE_BY_FORM.put(Form.HORNET, Summons.identifier("textures/entity/summon/fairy/hornet.png"));
+    TEXTURE_BY_FORM.put(Form.PROBOSCIS_FAIRY, Summons.identifier("textures/entity/summon/fairy/proboscis_fairy.png"));
+    TEXTURE_BY_FORM.put(Form.TIRAMISU, Summons.identifier("textures/entity/summon/fairy/tiramisu.png"));
+    TEXTURE_BY_FORM.put(Form.TIARA, Summons.identifier("textures/entity/summon/fairy/tiara.png"));
+    TEXTURE_BY_FORM.put(Form.COMET_STAR, Summons.identifier("textures/entity/summon/fairy/comet_star.png"));
   }
 
   private final FairySummonModel model;
@@ -54,9 +56,9 @@ public class FairySummonRenderer extends EntityRenderer<AbstractCompanion> {
   }
 
   @Override
-  public void render(AbstractCompanion cube, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
+  public void render(AbstractCompanion companion, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
 
-    if (cube.isWisp()) {
+    if (companion.isWisp()) {
       return; // no real renderer while wisp; AbstractCompanion#tick spawns particles instead
     }
 
@@ -69,13 +71,13 @@ public class FairySummonRenderer extends EntityRenderer<AbstractCompanion> {
     poseStack.scale(0.5f, 0.5f, 0.5f); // custom
     poseStack.scale(-1.0F, -1.0F, 1.0F); // BoatRenderer: invert coordinate space
 
-    this.model.setupAnim(cube, cube.walkAnimation.position(partialTick), Math.min(cube.walkAnimation.speed(partialTick), 1.0F),
-        cube.tickCount + partialTick, 0.0F, 0.0F);
+    this.model.setupAnim(companion, companion.walkAnimation.position(partialTick), Math.min(companion.walkAnimation.speed(partialTick), 1.0F),
+        companion.tickCount + partialTick, 0.0F, 0.0F);
     // entityCutoutNoCull, not entityCutout: the wings are thin double-sided planes that
     // rotate freely, and GL backface culling made one side of them disappear mid-flap
-    this.model.renderToBuffer(poseStack, bufferSource.getBuffer(RenderType.entityCutoutNoCull(DEFAULT_TEXTURE_LOCATION)), packedLight, OverlayTexture.NO_OVERLAY);
+    this.model.renderToBuffer(poseStack, bufferSource.getBuffer(RenderType.entityCutoutNoCull(getTextureLocation(companion))), packedLight, OverlayTexture.NO_OVERLAY);
     poseStack.popPose();
 
-    super.render(cube, entityYaw, partialTick, poseStack, bufferSource, packedLight);
+    super.render(companion, entityYaw, partialTick, poseStack, bufferSource, packedLight);
   }
 }
